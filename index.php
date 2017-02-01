2b29ba00a0cea6e728fa000b9cc383dbbedd7c7a
Intern <intern@IT2912.local>
2017-02-01 14:07:45 +0900

    third commit

--- index.php
+++ index.php
@@ -1,22 +1,8 @@
-<html>
-    <head>
-        <script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
-    </head>
-<body>
-
-<script>
-    function getUrlVars() {
-        var vars = {};
-        var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,    
-        function(m,key,value) {
-            vars[key] = value;
-        });
-        return vars;
+<?php
+    $a = $_GET['a']; 
+    $b = $_GET['b']; 
+    if(isset($a) && isset($b)) {
+        echo $a+$b;
     }
-    $( document ).ready(function() {
-        var x = getUrlVars();
-        document.write(parseInt(x.a) + parseInt(x.b));
-   });
-</script>
-</body>
-</html> 
+
+?>
