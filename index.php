a282fa29d45d144e78c24459e77d25c4a923ce69
Intern <intern@IT2912.local>
2017-02-01 14:11:19 +0900

    third commit

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
     $a = $_GET['a']; 
     $b = $_GET['b']; 
     if(isset($a) && isset($b)) {
-        echo $a+$b;
+        $sum = $a + $b;
+        $result = array('a' => $a, 'b' => $b, 'result' => $sum);
+        echo json_encode($result);
     }
-
 ?>
