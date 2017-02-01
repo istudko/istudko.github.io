6baaa9218d14f1d570d364afcedc44e84c04e03e
Teeranai <istudko@gmail.com>
2017-02-02 01:39:58 +0900

    toggle operation

--- index.php
+++ index.php
@@ -3,9 +3,8 @@
     $a = $_GET['a']; 
     $b = $_GET['b']; 
     if(isset($a) && isset($b)) {
-        $sum = $a * $b;
+        $sum = $a + $b;
         $result = array('a' => $a, 'b' => $b, 'result' => $sum);
         echo json_encode($result);
-        
     }
 ?>
